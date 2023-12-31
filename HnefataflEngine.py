edd13597ea5b0b3c7316d3c0b257869ddca22c13
snork <luizguilherme.lgrl@gmail.com>
2023-12-31 19:50:16 -0300

    Restricoes

--- HnefataflEngine.py
+++ HnefataflEngine.py
@@ -35,7 +35,30 @@ class GameState():
             move = self.moveLog.pop()  
             self.board[move.startRow][move.startCol]   = move.pieceMoved
             self.board[move.endRow][move.endCol] = move.pieceCaptured
-        
+            self.defenseToMove =  not (self.defenseToMove)
+    
+    def getValidMove(self):
+        return self.getAllPossibleMoves() 
+
+    def getAllPossibleMoves(self):
+        moves = [Move((0,3),(0,0),self.board)]
+        for r in range(len(self.board)):
+            for c in range(len(self.board[r])):
+                typePiece = self.board[r][c][0]
+                if (typePiece == "d" and self.defenseToMove) and (typePiece== "a"  and not self.defenseToMove):
+                    piece = self.board[r][c][1]
+                    if piece == "p":
+                        self.NormalPiecesMove(r,c,self.board)
+                elif(typePiece=="t" and self.defenseToMove):
+                    self.KingPieceMove()
+        return moves
+    
+    def NormalPiecesMove(self):
+        pass
+    def KingPieceMove(self):
+        pass
+    
+
 
 
 
@@ -53,9 +76,14 @@ class Move():
         self.startCol = startSq[1]
         self.endRow = endSq[0]
         self.endCol = endSq[1]        
-
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptured = board[self.endRow][self.endCol]
+        self.moveId = self.startRow*1000 +self.startCol+ self.endRow*100 + self.endCol
+    
+    def __eq__(self,other):
+        if isinstance(other,Move):
+            return self.moveId == other.moveId
+        return False
 
 
     def getHnefataflNotation(self):
--- HnefataflMain.py
+++ HnefataflMain.py
@@ -30,6 +30,8 @@ def main():
     running = True
     sqSelected= ()
     playerClicks = []
+    validMove = gs.getValidMove()
+    moved = False
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
@@ -52,13 +54,19 @@ def main():
                         pass
                     else:
                         move = HnefataflEngine.Move(playerClicks[0],playerClicks[1],gs.board)
-                        print(move.getHnefataflNotation())
-                        gs.makeMove(move)
+                        if move in validMove:
+                            print(move.getHnefataflNotation())
+                            gs.makeMove(move)
+                            moved = True
                     sqSelected = ()
                     playerClicks = []
             elif e.type == p.KEYDOWN:
                 if e.key == p.K_z:
                     gs.undoMove()
+                    moved = True
+            if moved:
+                validMove = gs.getValidMove()
+                moved = False
         
 
         DrawGameState(screen,gs)
